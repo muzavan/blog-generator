52068a0df2500c33a20ea80296d5f745e9a9d893
Muhammad Reza Irvanda <muzavan@gmail.com>
2017-05-13 20:37:17 +0700

    Add Main Program Example for Build Dictionary

--- Wordpress.Generator/Wordpress.Generator/Program.cs
+++ Wordpress.Generator/Wordpress.Generator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wordpress.Business.Generating;
 
 namespace Wordpress.Generator
 {
@@ -10,6 +11,10 @@ namespace Wordpress.Generator
     {
         static void Main(string[] args)
         {
+            var generator = new DictionaryBuilder();
+            generator.BuildDictionary();
+
+
         }
     }
 }
